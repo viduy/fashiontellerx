29a0da18f1147b74f5362bdc98e5c82bfdcbd21f
viduycheung <viduy@outlook.com>
2018-08-29 10:29:06 +0800

    svg animate

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,11 @@ const IndexPage = () => (
           Created by Viduy
         </p>
         <Link to="/page-2/">go to discover</Link>
+        <svg width="100%" height="172" viewBox="0 0 100% 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z" fill="white"/>
+        </svg>
+
+
       </div>
     
     </div>
@@ -19,3 +24,4 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
