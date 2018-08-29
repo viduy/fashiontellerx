b41229bc6aaa3d3a4d395494f5ed0ae216341b96
viduycheung <viduy@outlook.com>
2018-08-29 15:48:50 +0800

    .gitignore

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,15 @@ const IndexPage = () => (
           Created by Viduy
         </p>
         <Link to="/page-2/">go to discover</Link>
-        <svg width="100%" height="172" viewBox="0 0 100% 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div className="logos">
+          <img src={require('../images/logo-sketch.png')} width="50" alt="" />
+          <img src={require('../images/logo-figma.png')} width="50" alt="" />
+          <img src={require('../images/logo-studio.png')} width="50" alt="" />
+          <img src={require('../images/logo-framer.png')} width="50" alt="" />
+          <img src={require('../images/logo-react.png')} width="50" alt="" />
+          <img src={require('../images/logo-swift.png')} width="50" alt=""/>
+        </div>
+        <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill="white">
             <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="20s" values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
             M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29 34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0 233.161V87.1596Z;
