6425b795d77be9b415adb4fafa39ae7698669e9b
viduycheung <viduy@outlook.com>
2018-08-29 15:56:13 +0800

    rm .cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/viduy/Documents/dev/WebProject/reactX/src/layouts/index.js"
-  import data from "/Users/viduy/Documents/dev/WebProject/reactX/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
