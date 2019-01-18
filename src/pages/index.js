6d2dc243f71d078f9f0c36ec797f95d72026b860
viduycheung <viduy@icloud.com>
2019-01-18 18:48:37 +0800

    JSON静态数据

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,65 +4,123 @@ import Card from "../components/Card";
 import Section from "../components/Section";
 import Wave from "../components/Wave";
 import Layout from "../components/layout";
+import staticdata from "../../staticdata.json";
+import Cell from "../components/Cell";
+import styled from "styled-components";
+
+{staticdata.cells.map(cell => (
+    <Cell title={cell.title} image={cell.image}/>
+))}
+
+const SectionCaption = styled.p`
+    font-weight: 600;
+    font-size: 18px;
+    text-transform: uppercase;
+    color: #94A4BA;
+    text-align: center;
+`
+
+const sectionCellGroup = styled.div`
+    max-width: 800px;
+    margin: 0 auto 100px;
+    padding: 0 20px;
+    display: grid;
+    grid-template-columns: repeat(2, 1f);
+    grid-column-gap: 20px;
+
+    @media (max-width: 800px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`
+
 
 const IndexPage = () => (
-  <Layout>
-    <div>
-      <div className="Hero">
-        <div className="HeroGroup">
-          <h1>
-            Join in<br />Fashion Teller
-        </h1>
-          <p>
-            This is the center of fashion<br />
-            Created by Viduy
-        </p>
-          <Link to="/discover">go to discover</Link>
-          <div className="logos">
-            <img src={require("../images/logo-sketch.png")} width="50" alt="" />
-            <img src={require("../images/logo-figma.png")} width="50" alt="" />
-            <img src={require("../images/logo-studio.png")} width="50" alt="" />
-            <img src={require("../images/logo-framer.png")} width="50" alt="" />
-            <img src={require("../images/logo-react.png")} width="50" alt="" />
-            <img src={require("../images/logo-swift.png")} width="50" alt="" />
-          </div>
-          <Wave />
+    <Layout>
+        <div>
+            <div className="Hero">
+                <div className="HeroGroup">
+                    <h1>
+                        Join in<br />Fashion Teller
+                    </h1>
+                    <p>
+                        This is the center of fashion<br />
+                        Created by Viduy
+                    </p>
+                    <Link to="/discover">go to discover</Link>
+                    <div className="logos">
+                        <img
+                            src={require("../images/logo-sketch.png")}
+                            width="50"
+                            alt=""
+                        />
+                        <img
+                            src={require("../images/logo-figma.png")}
+                            width="50"
+                            alt=""
+                        />
+                        <img
+                            src={require("../images/logo-studio.png")}
+                            width="50"
+                            alt=""
+                        />
+                        <img
+                            src={require("../images/logo-framer.png")}
+                            width="50"
+                            alt=""
+                        />
+                        <img
+                            src={require("../images/logo-react.png")}
+                            width="50"
+                            alt=""
+                        />
+                        <img
+                            src={require("../images/logo-swift.png")}
+                            width="50"
+                            alt=""
+                        />
+                    </div>
+                    <Wave />
+                </div>
+            </div>
+            <div className="Cards">
+                <h2>New Comming</h2>
+                <div className="CardGroup">
+                    <Card
+                        title="React for Desingers"
+                        text="12 sections"
+                        image={require("../images/wallpaper.jpg")}
+                    />
+                    <Card
+                        title="React for Desingers"
+                        text="12 sections"
+                        image={require("../images/wallpaper.jpg")}
+                    />
+                    <Card
+                        title="React for Desingers"
+                        text="12 sections"
+                        image={require("../images/wallpaper.jpg")}
+                    />
+                    <Card
+                        title="React for Desingers"
+                        text="12 sections"
+                        image={require("../images/wallpaper.jpg")}
+                    />
+                </div>
+            </div>
+            <Section
+                image={require("../images/wallpaper2.jpg")}
+                logo={require("../images/logo-react.png")}
+                title="React for Designers"
+                text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
+            />
+            <SectionCaption>12 section - 6 hours</SectionCaption>
+            <sectionCellGroup>
+                {staticdata.cells.map(cell => (
+                    <Cell title={cell.title} image={cell.image} />
+                ))}
+            </sectionCellGroup>
         </div>
-      </div>
-      <div className="Cards">
-        <h2>New Comming</h2>
-        <div className="CardGroup">
-          <Card
-            title="React for Desingers"
-            text="12 sections"
-            image={require("../images/wallpaper.jpg")}
-          />
-          <Card
-            title="React for Desingers"
-            text="12 sections"
-            image={require("../images/wallpaper.jpg")}
-          />
-          <Card
-            title="React for Desingers"
-            text="12 sections"
-            image={require("../images/wallpaper.jpg")}
-          />
-          <Card
-            title="React for Desingers"
-            text="12 sections"
-            image={require("../images/wallpaper.jpg")}
-          />
-        </div>
-      </div>
-      <Section
-        image={require("../images/wallpaper2.jpg")}
-        logo={require("../images/logo-react.png")}
-        title="React for Designers"
-        text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
-      />
-    </div>
-  </Layout>
-  
+    </Layout>
 );
 
 export default IndexPage;
